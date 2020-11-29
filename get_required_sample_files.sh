28fcd7cf18f5ed1bdd4d48b9989fc730a7589023
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2020-11-29 20:40:00 +0530

    Update get_required_sample_files.sh

--- get_required_sample_files.sh
+++ get_required_sample_files.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 
-wget https://raw.githubusercontent.com/ansarigulshad/Datasets/master/fifa_19_players_data.csv
-wget https://raw.githubusercontent.com/ansarigulshad/Datasets/master/cities.csv
-wget https://raw.githubusercontent.com/ansarigulshad/Datasets/master/sample_07.csv
-wget https://raw.githubusercontent.com/ansarigulshad/Datasets/master/sample_08.csv
-wget https://raw.githubusercontent.com/ansarigulshad/Datasets/master/driver_data.zip
-unzip driver_data.zip
-sh data_generator.sh
+mkdir -p /var/tmp/hivesampledata
+
+wget -nv https://raw.githubusercontent.com/ansarigulshad/Datasets/master/fifa_19_players_data.csv -O /var/tmp/hivesampledata/fifa_19_players_data.csv
+wget -nv https://raw.githubusercontent.com/ansarigulshad/Datasets/master/cities.csv  -O /var/tmp/hivesampledata/cities.csv
+wget -nv https://raw.githubusercontent.com/ansarigulshad/Datasets/master/sample_07.csv  -O /var/tmp/hivesampledata/sample_07.csv
+wget -nv https://raw.githubusercontent.com/ansarigulshad/Datasets/master/sample_08.csv  -O /var/tmp/hivesampledata/sample_08.csv
+wget -nv https://raw.githubusercontent.com/ansarigulshad/Datasets/master/driver_data.zip  -O /var/tmp/hivesampledata/driver_data.zip
+
+unzip /var/tmp/hivesampledata/driver_data.zip -d /var/tmp/hivesampledata/
+
