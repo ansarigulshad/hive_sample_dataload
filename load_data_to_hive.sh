2b93cbee047f3ec40623e6a9426fa68746434c6b
Gulshad Ansari <5801838+ansarigulshad@users.noreply.github.com>
2020-11-29 21:07:28 +0530

    Update load_data_to_hive.sh

--- load_data_to_hive.sh
+++ load_data_to_hive.sh
@@ -5,11 +5,11 @@ _ZK_QUORUM=hdpcdp-1.vpc.cloudera.com:2181,hdpcdp-2.vpc.cloudera.com:2181,hdpcdp-
 _HIVE_CONNECTION_STRING="jdbc:hive2://$_ZK_QUORUM/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2"
 
 #Execute HiveQL Statements
-beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hivesampledata/cities.hiveql
-beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hivesampledata/emp_sampledb.hiveql
-beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hivesampledata/fifa.hiveql
-beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hivesampledata/hwx_sampledb.hiveql
-beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hivesampledata/sampledb.hiveql
+beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hive_sample_dataload/cities.hiveql
+beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hive_sample_dataload/emp_sampledb.hiveql
+beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hive_sample_dataload/fifa.hiveql
+beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hive_sample_dataload/hwx_sampledb.hiveql
+beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING   -f /var/tmp/hive_sample_dataload/sampledb.hiveql
 
 # beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING  -e 'show databases'
 # beeline -n hive -p hive -u $_HIVE_CONNECTION_STRING  -e 'select rank,name,population_2001,state from population.cities'
